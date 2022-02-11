efac896775b6f56b7c2b25bd5d452cb0dd49d5ed
Tejash11 <tejashth1103@gmail.com>
2022-02-11 23:58:16 +0530

    added login activity

--- app/src/main/java/com/example/vidyaverse/activityclass/MainActivity.java
+++ app/src/main/java/com/example/vidyaverse/activityclass/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.vidyaverse;
+package com.example.vidyaverse.activityclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.vidyaverse.R;
 
 public class MainActivity extends AppCompatActivity {
 
